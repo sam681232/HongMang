2068511797c371b8e7335182b9a3cc2a413599f3
sam <sam681232@gmail.com>
2021-08-23 17:19:57 +0800

    新建方案

---
+++ HongMang/HongMang/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using HongMang.App_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HongMang.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly TempEntities Db = new TempEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            var li = Db.F_Table.ToList();
+            return View(li);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ HongMang/HongMang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HongMang.Startup))]
+namespace HongMang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
